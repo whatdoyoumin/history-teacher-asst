b4d68f6dee57d1fe28ebd6ef5d99e538ea1db12a
whatdoyoumin <minling.goh91@gmail.com>
2025-03-09 22:14:34 +0800

    update

--- test.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-# Load the processed PDF documents
-with open('data/textbooks.pkl', 'rb') as f:
-    pdf_documents = pickle.load(f)
-
-# Print metadata of the first document to check
-print(pdf_documents[0].metadata)
